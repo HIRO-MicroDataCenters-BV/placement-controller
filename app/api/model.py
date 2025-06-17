574ae8e08f8f864c427430324110863dd5f9d3dd
ktatarnikov <kostyantyn.tatarnikov@gmail.com>
2025-06-17 10:26:18 +0200

    [api] set owner api

--- app/api/model.py
+++ app/api/model.py
@@ -8,10 +8,12 @@ from app.core.application import Application
 class ApplicationModel(BaseModel):
     name: str
     namespace: str
+    owner: str
     zones: List[str]
 
     @staticmethod
     def from_object(application: Application) -> "ApplicationModel":
         name = application.get_namespaced_name()
         zones = application.get_placement_zones()
-        return ApplicationModel(name=name.name, namespace=name.namespace, zones=zones)
+        owner = application.get_owner_zone() or ""
+        return ApplicationModel(name=name.name, namespace=name.namespace, zones=zones, owner=owner)

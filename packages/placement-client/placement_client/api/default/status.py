b066ecb2fdd1fb63570214d0f8ac99103d13948e
ktatarnikov <kostyantyn.tatarnikov@gmail.com>
2025-09-18 10:10:22 +0200

    EDGE-304 improving current API

--- packages/placement-client/placement_client/api/default/status.py
+++ packages/placement-client/placement_client/api/default/status.py
@@ -46,6 +46,8 @@ def sync_detailed(
 ) -> Response[Any]:
     """Root
 
+     Get Application Status
+
     Raises:
         errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
         httpx.TimeoutException: If the request takes longer than Client.timeout.
@@ -68,6 +70,8 @@ async def asyncio_detailed(
     client: Union[AuthenticatedClient, Client],
 ) -> Response[Any]:
     """Root
+
+     Get Application Status
 
     Raises:
         errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.

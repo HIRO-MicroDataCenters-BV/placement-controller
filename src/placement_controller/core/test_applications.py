237c9a882d612bfaf3bbf2599b4e0ae07775e159
ktatarnikov <kostyantyn.tatarnikov@gmail.com>
2025-10-08 11:23:11 +0200

    EDGE-287 Applications

---
+++ src/placement_controller/core/test_applications.py
@@ -0,0 +1,32 @@
+import asyncio
+
+from placement_controller.async_fixture import AsyncTestFixture
+from placement_controller.clients.k8s.client import NamespacedName
+from placement_controller.clients.k8s.fake_client import FakeClient
+from placement_controller.core.applications import Applications
+from placement_controller.resource_fixture import ResourceTestFixture
+from placement_controller.settings import PlacementSettings
+
+
+class ApplicationsTest(AsyncTestFixture, ResourceTestFixture):
+    task: asyncio.Task[None]
+
+    name: NamespacedName
+    client: FakeClient
+    settings: PlacementSettings
+
+    def setUp(self) -> None:
+        super().setUp()
+        self.client = FakeClient()
+        self.name = NamespacedName(name="test", namespace="testns")
+        self.settings = PlacementSettings(namespace="test", available_zones=["zone1", "zone2"], current_zone="zone1")
+
+        self.applications = Applications(self.client, self.terminated, self.settings)
+        self.task = self.loop.create_task(self.applications.run())
+
+    def tearDown(self) -> None:
+        super().tearDown()
+        self.task.cancel()
+
+    def test_applications(self) -> None:
+        pass

db7c14c2da76e567e686d2d98b2731db816053a8
ktatarnikov <kostyantyn.tatarnikov@gmail.com>
2025-10-21 15:25:38 +0200

    EDGE-309 DecisionStore

---
+++ src/placement_controller/store/types.py
@@ -0,0 +1,16 @@
+from typing import List
+
+from placement_controller.clients.k8s.client import NamespacedName
+
+
+class DecisionStore:
+
+    async def save(
+        self,
+        name: NamespacedName,
+        placement: List[str],
+        reason: str,
+        trace: str,
+        timestamp: int,
+    ) -> None:
+        raise NotImplementedError
